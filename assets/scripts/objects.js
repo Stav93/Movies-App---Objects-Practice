1bd62fdeb3bbc04692e4548ab625d42b3bf6b0a1
Stav <stavush93@gmail.com>
2022-08-05 17:51:52 +0300

    displaying the movies is the UI

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -3,6 +3,27 @@ const serchBtn = document.getElementById("serch-btn");
 
 const movies = [];
 
+const displayingMovies = () => {
+  const movieList = document.getElementById("movie-list");
+
+  // the idile way is to not clear the html but append each new movie to the exiting elemnt
+
+  if (movies.length === 0) {
+    movieList.classList.remove("visible");
+    return;
+  } else {
+    movieList.classList.add("visible");
+  }
+  movieList.innerHTML = "";
+
+  movies.forEach( movie => {
+    const movieEl = document.createElement("li");
+    movieEl.textContent = movie.info.title;
+    movieList.append(movieEl);
+  })
+
+}
+
 const addMovieHandler = () => {
   const titleValue = document.getElementById("title").value;
   const extraNameValue = document.getElementById("extra-name").value;
@@ -24,6 +45,7 @@ const addMovieHandler = () => {
   }
 
   movies.push(newMovie)
+  displayingMovies()
 };
 
 addMovieBtn.addEventListener("click", addMovieHandler)
